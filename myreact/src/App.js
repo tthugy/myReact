ac558f6852bbc34f86186c0a9113a683ff491f9c
tthugy <supermanace7@gmail.com>
2022-04-11 00:43:27 +0900

    To Do List 3

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
 
@@ -10,16 +11,23 @@ function App() {
     if(toDo === ""){
       return;
     }
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
-    setToDos(currentArray => [toDo, ...currentArray]);
-  }
-
+  };
+  console.log(toDos);
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+      {toDos.map((item, index) => (
+        <li key={index} className={styles.list}>{item}</li>
+      ))}
+      </ul>
     </div>
   );
 }
