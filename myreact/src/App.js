9df565dbee567fb1599d59e0d91479a9687845d0
tthugy <supermanace7@gmail.com>
2022-04-10 23:51:25 +0900

    To Do List

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,23 +1,23 @@
 import { useState, useEffect } from "react";
 
-function Hello(){
-  useEffect(() =>{
-    console.log("created :)");
-    return () => console.log("destroyed :(");
-  }, [])
-  return (
-    <h1>Hello</h1>
-  );
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDo("");
+  }
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
