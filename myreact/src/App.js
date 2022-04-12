cef98a1b45a8001e6879db6600ea85075109a9f1
tthugy <supermanace7@gmail.com>
2022-04-12 22:59:16 +0900

    Movie App2

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -9,12 +9,15 @@ function App() {
   
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year`
+    )).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
-    .then((response) => response.json()
-    .then(json => setMovies(json.data.movies)));
+    getMovies();
   }, [])
   console.log(movies);
 
@@ -22,7 +25,20 @@ function App() {
     <div>
       {loading ? <h1>Loading...</h1> : 
       <div>
-        api
+        {movies.map(movie =>
+        <div>
+          <img src={movie.medium_cover_image} />
+          <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map(g => 
+              <li key={g}>{g}</li>
+              )}
+            </ul>
+          </div>
+        </div>
+        )}
       </div>
       }
     </div>
