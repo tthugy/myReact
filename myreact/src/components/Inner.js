4a27cfe9258958718c93b73eadc608a07501149e
tthugy <supermanace7@gmail.com>
2022-04-14 23:11:00 +0900

    update movie

---
+++ myreact/src/components/Inner.js
@@ -0,0 +1,11 @@
+// import { Link } from "react-router-dom";
+
+function Inner(){
+    return (
+      <div>
+        1212213
+      </div>
+    );
+}
+
+export default Inner;
--- myreact/src/routes/Detail.js
+++ myreact/src/routes/Detail.js
@@ -1,18 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Inner from "../components/Inner.js";
 
 function Detail(){
     const {id} = useParams();
-    const getMovie = async () => {
+    const [details, setDetails] = useState([]);
+    const getMovie = async() => {
         const json = await(
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
-        console.log(json);
-    }
+        setDetails(json.data.movie);
+    };
     useEffect(() => {
         getMovie();
     }, []);
-    return <h1>Detail</h1>
+    console.log(details);
+    return (
+        <div>
+          {details.map((detail) => (
+            <Inner
+            key={detail.id}/>
+          ))}
+        </div>
+    );
 }
 
 export default Detail;
